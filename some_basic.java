16a8c5fdfc4501340c5d12aaf7e9c3384c030161
Saishab Bhattarai <saishab.bhattarai.01@gmail.com>
2024-08-19 20:48:06 +0545

    Basic commands in Java

---
+++ some_basic.java
@@ -0,0 +1,28 @@
+public class some_basic {
+    public static void main(String[] args) {
+        /*
+         working with the String Variable
+         */
+        String name = "Balaram Jayanti";
+        System.out.println(name);
+          /*
+         working with the Integer Variable
+         */
+        int myNum = 5000;
+        System.out.println(myNum);
+        System.out.println("Hello, today is \" " + name + " \" and its " + myNum + " years of celebration.");
+
+        /*
+         working with multiple variables
+         */
+
+         int x = 5;
+         int y = 9;
+         int z = 10;
+        System.out.println("The final output is "+ (x+y+z));
+
+    }
+}
+
+
+
