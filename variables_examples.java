db9acc49e9e107cd59546fd118833cbf16cdc3b3
Saishab Bhattarai <saishab.bhattarai.01@gmail.com>
2024-08-19 21:11:16 +0545

    Variables in Java

---
+++ variables_examples.java
@@ -0,0 +1,13 @@
+public class variables_examples {
+
+    public static void main(String[] args) {
+        
+        int Principle = 5000;
+        float rate = 8;
+        float time = 8;
+
+        float Simple_interest = (Principle*rate*time)/100;
+
+        System.out.println(Simple_interest);
+    }
+}
